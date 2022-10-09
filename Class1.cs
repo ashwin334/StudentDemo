ea464a43d0e66b7ee62922cf8fd26b75bc9b39dd
ashwin333 <ashwinpatil832@gmail.com>
2022-10-09 22:23:21 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo
+{
+    internal class Student
+    {
+        private int rollno;
+        private string name;
+        double percentage;
+
+        public void add(int rollno,string name,double percentage)
+        {
+            this.rollno = rollno;
+            this.name = name;   
+            this.percentage = percentage;   
+        }
+        public string Print()
+        {
+            return $" Student;{rollno}/{name}/{percentage}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.add(121,"Ashwin", 75.7);
+            Console.WriteLine(s1.Print());
+        }
+    }
+}
